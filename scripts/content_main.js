197334fac7e979bf0a7f05336543d4e3aceb4676
Natsuki Kanai <natsuki-kanai@kccs.co.jp>
2025-07-18 15:16:22 +0000

    feat: Shortcut menu

--- scripts/content_main.js
+++ scripts/content_main.js
@@ -1,15 +1,43 @@
 // ****************************************************
-// ***** ホームメニュー画面、ログイン画面あたりの処理 *****
+// ホームメニュー画面、ログイン画面、全画面共通の処理
 // ****************************************************
 
-$(function() {
+function renderShortcutMenu() {
+    if (TheAmoebaSupportSetting.isChoiceShortcutMenu) {
+        const header = $('#Header_Page');
+        const shortcutBar = $('#TheAmoebaSupport_ShortcutBar');
+
+        if (header.length > 0 && shortcutBar.length == 0) {
+            const shortcutMenuBar = $('<div id="TheAmoebaSupport_ShortcutBar">')
+            $('#Header_Page').after(shortcutMenuBar);
+            $('#Amoeba_Page').css('margin-top', '20px');
+
+            TheAmoebaSupportSetting.shortcutMenuList.forEach(function (sm) {
+                const shortcutMenuItem = $('<div class="theamoebasupport-shortcut-menu-item">');
+                // menutype="3"
+                shortcutMenuItem.append($('<a onclick="sendMenuList(this);">').attr('id', sm.programId).text(sm.displayName));
+                shortcutMenuBar.append(shortcutMenuItem);
+            });
+        }
+    }
+}
+
+$(function () {
     $('div.Title_Form input[name="CompanyCD"]').val(TheAmoebaSupportSetting.companyCode);
+    renderShortcutMenu();
 })
 
+// ページによって描画タイミングが異なるため、MutationObserver も入れておく。
+const observer = new MutationObserver(() => {
+    renderShortcutMenu();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
 // ホームメニュー画面でタイルの中心のアイコンだけではなく、タイル全体をクリックできるようにする。
-$(document).on('click', 'td.Menu_Picture_Area.menu_cursor_sender', function() {
+$(document).on('click', 'td.Menu_Picture_Area.menu_cursor_sender', function () {
     var elem = $(this).find('a.Common_Menu_Class')[0];
-    if(elem) {
+    if (elem) {
         elem.click();
     }
 });
--- scripts/options.js
+++ scripts/options.js
@@ -1,53 +1,106 @@
+function appendRow(tableBody) {
+  const addTargetTableRow = tableBody.find('tr:first-child');
+  const addTableRow = addTargetTableRow.clone(true);
+  addTableRow.find("input").val("");
+  tableBody.append(addTableRow);
+  return addTableRow;
+}
 
-$(function(){
+$(function () {
 
-  $('#choice_performer_history_off').prop('checked', true);
-  $('#choice_enter_search_off').prop('checked', true);
+  $('#choice_shortcut_menu').prop('checked', false);
+  $('#choice_performer_history').prop('checked', false);
+  $('#choice_enter_search').prop('checked', false);
 
   // オプション画面の初期値を設定する
   chrome.storage.local.get("the_amoeba_support_setting", function (value) {
     var d = value.the_amoeba_support_setting;
-    if(d) {
+    if (d) {
       if (d.company_code) {
         $('#company_code').val(d.company_code);
       }
 
+      if (d.choice_shortcut_menu) {
+        $('#choice_shortcut_menu').prop('checked', true);
+      }
+
       if (d.choice_performer_history) {
-        $('#choice_performer_history_on').prop('checked', true);
-      } else {
-        $('#choice_performer_history_off').prop('checked', true);
+        $('#choice_performer_history').prop('checked', true);
       }
 
       if (d.choice_enter_search) {
-        $('#choice_enter_search_on').prop('checked', true);
-      } else {
-        $('#choice_enter_search_off').prop('checked', true);
+        $('#choice_enter_search').prop('checked', true);
       }
 
+      if (d.shortcut_menu_list) {
+        const tableBody = $("#shortcut_menu_list .flex-table table tbody");
+
+        d.shortcut_menu_list.forEach(function (sm) {
+          const row = tableBody.find('tr:last-child')
+          row.find('input[name="program-id"]').val(sm.programId);
+          row.find('input[name="display-name"]').val(sm.displayName);
+
+          appendRow(tableBody);
+        })
+      }
     }
   });
-  
+
 });
 
 // セーブボタンが押されたら、
 // ストレージに保存する。
 $(document).on("click", "#save", function () {
-  
+
   var data = {};
 
   data.company_code = $('#company_code').val();
 
-  if ($('#choice_performer_history_on').prop('checked')) {
+  if ($('#choice_shortcut_menu').is(':checked')) {
+    data.choice_shortcut_menu = true;
+  } else {
+    data.choice_shortcut_menu = false;
+  }
+
+  if ($('#choice_performer_history').is(':checked')) {
     data.choice_performer_history = true;
   } else {
     data.choice_performer_history = false;
   }
 
-  if ($('#choice_enter_search_on').prop('checked')) {
+  if ($('#choice_enter_search').is(':checked')) {
     data.choice_enter_search = true;
   } else {
     data.choice_enter_search = false;
   }
-  
-  chrome.storage.local.set({"the_amoeba_support_setting": data} , function(){});
+
+  data.shortcut_menu_list = [];
+  $('#shortcut_menu_list .flex-table table tbody tr').each(function (index, element) {
+    const programId = $(element).find('input[name="program-id"]').val().trim();
+    const dsplayName = $(element).find('input[name="display-name"]').val().trim();
+
+    if (!programId && !dsplayName) return;
+
+    data.shortcut_menu_list.push({
+      programId: programId,
+      displayName: dsplayName
+    })
+  });
+
+  chrome.storage.local.set({ "the_amoeba_support_setting": data }, function () { });
+});
+
+
+// テーブルの行追加
+$(document).on("click", ".flex-table .add-btn", function () {
+  const tableBody = $(this).closest(".flex-table").find("table tbody");
+  appendRow(tableBody)
+});
+
+// テーブルの行削除
+$(document).on("click", ".flex-table .remove-btn", function () {
+  const tableBody = $(this).closest(".flex-table").find("table tbody");
+  if (tableBody.children().length > 1) {
+    $(this).closest("tr").remove();
+  }
 });
